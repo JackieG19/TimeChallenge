039fb3ebec455aa0404dcdda2791fcb7627de40b
Jackie <JGtheophila19@gmail.com>
2019-10-29 12:19:30 -0400

    reset 1hr: solved in 32 minutes
    passed all test case

--- spinalCase.js
+++ spinalCase.js
@@ -25,15 +25,31 @@ function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
   
-  var res = str.replace(/\s/g, '-').toLowerCase();
-  //str.toLowerCase().trim().split(/\s+/).join('-');
-  //str.toLowerCase();
-  console.log(res);
+    // lower case all the words
+    // var res = str.toLowerCase(); 
+    // console.log(res);
   
-  return str;
+    // removed underscore for output 3
+    // var remove = res.replace(/_/g, " "); 
+    // console.log(remove);
+  
+    // adds the dash in between the words
+    // var newStr = remove.replace(/\s+/g, '-'); 
+    // console.log(newStr);
+  
+    // adds the space in between the words for output 2 and 5
+    // var removeSp = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+    // console.log(removeSp);
+
+    var replace = str.replace(/_/g, " ").replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
+    //console.log(replace);
+
+    var newStr = replace.replace(/\s+/g, '-');
+    //console.log(newStr);
+    
+    return newStr;
 }
-spinalCase("The_Andy_Griffith_Show");
-
+//spinalCase("thisIsSpinalTap");
 //spinalCase("AllThe-small Things");
-
-//spinalCase('This Is Spinal Tap');
+//spinalCase("The_Andy_Griffith_Show");
+spinalCase('This Is Spinal Tap');
