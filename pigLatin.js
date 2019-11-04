1c980056bcc0b04adbba0f2ba0a84c878a0f8bfe
Jackie <JGtheophila19@gmail.com>
2019-11-04 10:34:23 -0500

    time: 1hr & 14 minutes - 6/7 test case passed

--- pigLatin.js
+++ pigLatin.js
@@ -28,26 +28,37 @@ Should handle words without vowels.
 translatePigLatin("rhythm") should return "rhythmay".
 */
 
+// function translatePigLatin(str) {
+//     /*If a word begins with a vowel(a,e,i,o,u)
+//         add "way" to the end.*/
+
+//     var vowel = str.replace(/[^aeiou]/gi, "").length;
+//     console.log(vowel);
+
+//     var strlen = str.length;
+//     console.log(strlen);
+
+//     for(var s = 0; s < strlen; s++){
+
+//     }
+
+
 function translatePigLatin(str) {
-    /*If a word begins with a vowel(a,e,i,o,u)
-        add "way" to the end.*/
-
-    var vowel = str.replace(/[^aeiou]/gi, "").length;
-    console.log(vowel);
-
-    var strlen = str.length;
-    console.log(strlen);
-
-    for(var s = 0; s < strlen; s++){
-
-    }
-
-     /*If a word does not contain a vowel
+    
+    /*If a word does not contain a vowel
         add "ay" to the end.*/
     // Input strings are all lowercase.
 
-
-
+  var vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.indexOf(str.charAt(0)) > -1) {
+    return str + "way";
+  }
+  for (var i = 1; i < str.length; i++) {
+    if (vowels.indexOf(str.charAt(i)) > -1) {
+      str = str.slice(i) + str.slice(0, i) + "ay";
+      return str;
+    }
+  }
     // should return "onsonantcay"
   return str;
 }
